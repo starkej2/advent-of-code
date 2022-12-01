a67c8e2c668fa582c1b7c44a45da424fd544fff3
Jeffrey Starke <dev@jeffreystarke.me>
2022-12-01 15:56:04 -0500

    Solve day 1, part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,41 @@
 private const val DAY_NAME = "Day01"
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("${DAY_NAME}_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput(DAY_NAME)
     println(part1(input))
-    println(part2(input))
 }
+
+private fun part1(input: List<String>): Int {
+    val elfHauls = parseInput(input)
+    return elfHauls.maxOf { it.totalCalories }
+}
+
+private fun parseInput(input: List<String>): List<ElfHaul> {
+    val elfHauls = mutableListOf<ElfHaul>()
+
+    val iterator = input.iterator()
+    while (iterator.hasNext()) {
+        val foodItems = mutableListOf<FoodItem>()
+        do {
+            val nextInput = iterator.next().toIntOrNull()
+            if (nextInput != null) {
+                foodItems.add(FoodItem(nextInput))
+            }
+        } while (iterator.hasNext() && nextInput != null)
+
+        elfHauls.add(ElfHaul(foodItems))
+    }
+
+    return elfHauls
+}
+
+data class ElfHaul(
+    val foodItems: List<FoodItem>,
+) {
+    val totalCalories: Int = foodItems.sumOf { it.calories }
+}
+
+@JvmInline
+value class FoodItem(val calories: Int)
